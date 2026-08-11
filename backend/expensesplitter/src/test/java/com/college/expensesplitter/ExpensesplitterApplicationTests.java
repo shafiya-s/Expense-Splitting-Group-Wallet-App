06134ea74eab4db8c9031c3ecb0f4edee94f4953
shafiya-s <shafiyas2152@gmail.com>
2026-08-11 21:37:12 +0530

    chore: add Spring Boot project foundation

---
+++ backend/expensesplitter/src/test/java/com/college/expensesplitter/ExpensesplitterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.college.expensesplitter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpensesplitterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
