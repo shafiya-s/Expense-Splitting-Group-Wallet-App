bf8e3439b912d366bb59e41201d3c60891e2e114
shafiya-s <shafiyas2152@gmail.com>
2026-08-11 21:23:13 +0530

    feat(repository): add user repository

---
+++ backend/expensesplitter/src/main/java/com/college/expensesplitter/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.college.expensesplitter.repository;
+
+import com.college.expensesplitter.model.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+    boolean existsByEmail(String email);
+}
