feb17200aa2cd5734e29f805a8f7897a121e0ace
shafiya-s <shafiyas2152@gmail.com>
2026-09-15 12:17:09 +0530

    feat(group): update group member repository with query methods

--- backend/expensesplitter/src/main/java/com/college/expensesplitter/repository/GroupMemberRepository.java
+++ backend/expensesplitter/src/main/java/com/college/expensesplitter/repository/GroupMemberRepository.java
@@ -1,7 +1,16 @@
 package com.college.expensesplitter.repository;
 
+import com.college.expensesplitter.model.entity.Group;
 import com.college.expensesplitter.model.entity.GroupMember;
+import com.college.expensesplitter.model.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
-public interface GroupMemberRepository extends JpaRepository<GroupMember,Long> {
+public interface GroupMemberRepository extends JpaRepository<GroupMember, Long> {
+
+    List<GroupMember> findByUser(User user);
+
+    List<GroupMember> findByGroup_Id(Long groupId);
+
+    boolean existsByGroupAndUser(Group group, User user);
 }
